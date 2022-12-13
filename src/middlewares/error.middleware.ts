e0ba7803fdc010da94a51710f31ba448a96f069a
Ray <raylu511@gmail.com>
2022-12-12 22:50:17 -0500

    Created HTTP exception class and created error handling middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+import { HttpException } from '@/exceptions/HttpException';
+
+const errorMiddleware = (error: HttpException, _req: Request, res: Response, next: NextFunction) => {
+  try {
+    const status = error.status || 500;
+    const message = error.message || 'Something went wrong';
+    res.status(status).json({ message });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
