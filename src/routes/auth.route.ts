d11b9a1676a92bbe77ba119dc49aa14379eae301
Ray <73660317+raylu511@users.noreply.github.com>
2022-12-14 21:33:28 -0500

    1 create signup route (#12)
    * Create auth.route.ts
    
    * Create validation.middleware.ts
    
    * Created auth and user interfaces
    
    * Create users.entity.ts
    
    * Create users.dto.ts
    
    * Finished Signup Route
    
    * Update auth.service.ts

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { Routes } from '@/interfaces/routes.interface';
+import validationMiddleware from '@/middlewares/validation.middleware';
+import { CreateUserDto } from '@/dtos/users.dto';
+import AuthController from '@/controllers/auth.controller';
+
+class AuthRoute implements Routes {
+  public readonly path = '/';
+  public readonly router = Router();
+  private authController = new AuthController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}signup`,
+      validationMiddleware({
+        type: CreateUserDto,
+        value: 'body',
+      }),
+      this.authController.signUp,
+    );
+  }
+}
+
+export default AuthRoute;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import App from '@/app';
 import validateEnv from '@utils/validateEnv';
 import IndexRoute from './routes/index.route';
+import AuthRoute from './routes/auth.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute()]);
 
 app.listen();
