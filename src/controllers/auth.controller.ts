b86b2c6a66482cd538278aca6e96ed85f35e8dd8
Ray <raylu511@gmail.com>
2022-12-17 16:04:39 -0500

    Created Login endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,11 +6,24 @@ import AuthService from '@/services/auth.service';
 class AuthController {
   public authService = new AuthService();
 
-  public signUp = async (req: Request, res: Response, next: NextFunction) => {
+  public register = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userData: CreateUserDto = req.body;
-      const { id }: User = await this.authService.signup(userData);
+      const { id }: User = await this.authService.register(userData);
       res.status(201).json({ id, message: 'success' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userData: CreateUserDto = req.body;
+      const {
+        cookie,
+        foundUser: { id },
+      } = await this.authService.login(userData);
+      res.setHeader('Set-Cookie', [cookie]).status(200).json({ id, message: 'success' });
     } catch (error) {
       next(error);
     }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,50 @@
-import { hash } from 'bcrypt';
-// import { sign } from 'jsonwebtoken';
-import { EntityRepository } from 'typeorm';
-// import { SECRET_KEY } from '@config';
+import { compare, hash } from 'bcrypt';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from '@dtos/users.dto';
-// import { HttpException } from '@exceptions/HttpException';
 import { User } from '@interfaces/users.interface';
-// import { DataStoredInToken, TokenData } from '@interfaces/auth.interface';
 import { UserEntity } from '@entities/users.entity';
 import { HttpException } from '@exceptions/HttpException';
+import { DataStoredInToken, TokenData } from '@/interfaces/auth.interface';
+import { SECRET_KEY } from '@/config';
+import { sign } from 'jsonwebtoken';
 
-@EntityRepository()
-class AuthService {
-  public async signup({ password, ...userData }: CreateUserDto): Promise<User> {
-    const findUser: User = await UserEntity.findOne({ where: { email: userData.email } });
-    if (findUser) throw new HttpException(409, `This email ${userData.email} already exists`);
+class AuthService extends Repository<UserEntity> {
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    super();
+  }
+
+  private createToken({ id }: User): TokenData {
+    const dataStoredInToken: DataStoredInToken = { id };
+    const expiresIn = 100;
+    return { expiresIn, token: sign(dataStoredInToken, SECRET_KEY, { expiresIn }) };
+  }
+
+  private createCookie(tokenData: TokenData): string {
+    return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
+  }
+
+  public async register({ password, ...userData }: CreateUserDto): Promise<User> {
+    const foundUser: User = await UserEntity.findOne({ where: { email: userData.email } });
+    if (foundUser) throw new HttpException(409, `This email ${userData.email} already exists`);
     const hashedPassword = await hash(password, 10);
     const createUserData: User = await UserEntity.create({ ...userData, password: hashedPassword }).save();
     return createUserData;
+  }
+
+  public async login({ email, password }: CreateUserDto): Promise<{ cookie: string; foundUser: User }> {
+    const foundUser: User = await UserEntity.findOne({ where: { email: email } });
+    if (!foundUser) {
+      throw new HttpException(404, `This email ${email} doesn't exist`);
+    }
+    const isCorrectPassword = await compare(password, foundUser.password);
+    if (!isCorrectPassword) {
+      throw new HttpException(409, 'Password does not match');
+    }
+    const token = this.createToken(foundUser);
+    const cookie = this.createCookie(token);
+    return { cookie, foundUser };
   }
 }
 
