512c29a648cef80d7b77fece756361386772a333
Daywick <derekli11204@gmail.com>
2022-12-26 16:00:50 -0500

    started post endpoint

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,3 +1,4 @@
 export { LoginUserRequestDto, RegisterUserRequestDto, AuthenticateUserRequestDto, RegisterUserResponseDto, BaseUserDto, LoginUserResponseDto } from './users.dto';
 export { BaseCommentDto } from './comments.dto';
+export { BasePostDto } from './posts.dto';
 
