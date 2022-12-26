23bafb2f5e09ed19289ac91bbb4eb36b87c48117
Matt-Tan15 <tanmatt1515@gmail.com>
2022-12-26 18:29:29 -0500

    created bidirectional relationship

---
+++ src/entities/connections.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import UserEntity from './users.entity';
+
+@Entity()
+class ConnectionsEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @ManyToOne(() => UserEntity, (user) => user.id)
+    sender: UserEntity;
+    
+    @ManyToOne(() => UserEntity, (user) => user.connections)
+    receiver: UserEntity;
+    
+    @Column()
+    connected: boolean;
+
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
+
+export default ConnectionsEntity;
