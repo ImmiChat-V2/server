9e64fe9468a1955b2f1cfd333253e2889cf74a0c
Matthew Tan <tanmatt1515@gmail.com>
2022-12-26 18:46:33 -0500

    removed duplicate files and exported connections from indexts

--- src/entities/sql/index.ts
+++ src/entities/sql/index.ts
@@ -1,4 +1,4 @@
 export { default as UserEntity } from './users.entity';
 export { default as CommentEntity } from './comments.entity';
 export { default as PostEntity } from './posts.entity';
-
+export { default as ConnectionsEntity } from './connections.entity';
