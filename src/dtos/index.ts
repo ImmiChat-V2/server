009261254d2d4b20986232a5e1096212856ea713
Matt-Tan15 <tanmatt1515@gmail.com>
2022-12-26 03:28:59 -0500

    created connections entity and base connections dto

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,3 +1,3 @@
 export { LoginUserRequestDto, RegisterUserRequestDto, AuthenticateUserRequestDto, RegisterUserResponseDto, BaseUserDto, LoginUserResponseDto } from './users.dto';
 export { BaseCommentDto } from './comments.dto';
-
+export { BaseConnectionsDto } from './connections.dto';
--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -1,4 +1,6 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, OneToMany, JoinTable } from 'typeorm';
+import CommentEntity from './comments.entity';
+import ConnectionsEntity from './connections.entity';
 
 @Entity()
 class UserEntity extends BaseEntity {
@@ -34,6 +36,9 @@ class UserEntity extends BaseEntity {
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  @OneToMany(() => ConnectionsEntity, (connection) => connection.id)
+  connections: CommentEntity[];
 }
 
 export default UserEntity;
