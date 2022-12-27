1866c21f321cc81fa2513344db444014ad578788
Matthew Tan <tanmatt1515@gmail.com>
2022-12-27 18:25:11 -0500

    rebased

---
+++ src/entities/users.entity.ts
@@ -0,0 +1,45 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, OneToMany, JoinTable } from 'typeorm';
+import ConnectionsEntity from './sql/connections.entity';
+
+@Entity()
+class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  @Unique(['email'])
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  language: string;
+
+  @Column({ nullable: true })
+  dateOfBirth: Date;
+
+  @Column({ nullable: true })
+  profilePic: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => ConnectionsEntity, connection => connection.sender)
+  senderId: ConnectionsEntity[];
+
+  @OneToMany(() => ConnectionsEntity, connection => connection.receiver)
+  receiverId: ConnectionsEntity[];
+}
+
+export default UserEntity;
