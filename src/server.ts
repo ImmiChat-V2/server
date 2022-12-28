1ae89d400851e45a98d4a398b5dd859a655d7d49
Daywick <derekli11204@gmail.com>
2022-12-28 04:22:32 -0500

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import App from '@/app';
 import validateEnv from '@utils/validateEnv';
-import { IndexRoute, AuthRoute, UserRoute, PostRoute } from '@/routes';
+import { IndexRoute, AuthRoute, UserRoute, PostRoute, CommentRoute } from '@/routes';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute(), new PostRoute(), new UserRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new PostRoute(), new UserRoute(), new CommentRoute()]);
 
 app.listen();
