c9ed0da2455d30ac35df2ad3072ccb7da848f6df
Ray <raylu511@gmail.com>
2022-12-28 18:29:48 -0500

    Update queryBuilderUtils.ts

--- src/utils/queryBuilderUtils.ts
+++ src/utils/queryBuilderUtils.ts
@@ -1,4 +1,8 @@
-export const updateAndReturn = async <ReturnType, PayloadType>(id: number, data: PayloadType, Entity): Promise<ReturnType> => {
+import { BaseEntity } from 'typeorm';
+
+type BaseEntityType = typeof BaseEntity;
+
+export const updateAndReturn = async <ReturnType, PayloadType>(id: number, data: PayloadType, Entity: BaseEntityType): Promise<ReturnType> => {
   const updatedRow = await Entity.createQueryBuilder().update().where({ id }).set(data).returning('*').execute();
   return updatedRow.raw[0];
 };
