42028cf53a9ed817ec84eb2e617524298466ad98
Ray <raylu511@gmail.com>
2022-12-29 17:42:15 -0500

    Feed

--- src/dtos/feed.dto.ts
+++ src/dtos/feed.dto.ts
@@ -1,15 +1,12 @@
 export type BasePostOfFeedDTO = {
   readonly id: number;
+  readonly userId: number;
   readonly content: string;
   readonly media?: string;
-  readonly userId: number;
-  readonly firstName: string;
-  readonly lastName: string;
-  readonly profilePic?: string;
   readonly likes: {
     readonly firstName: string;
     readonly lastName: string;
-    readonly profilePic: string;
+    readonly profilePic?: string;
   };
   readonly comments: {
     readonly userId: number;
--- src/services/feed.service.ts
+++ src/services/feed.service.ts
@@ -1,11 +1,12 @@
+import { BasePostOfFeedDTO } from '@/dtos/feed.dto';
 import { PostEntity } from '@/entities';
 
 class FeedService {
-  public async getFeed() {
-    const feed = await PostEntity.find({
+  public async getFeed(): Promise<BasePostOfFeedDTO[]> {
+    const feed: BasePostOfFeedDTO[] = (await PostEntity.find({
       relations: ['likes', 'comments'],
       select: { likes: { firstName: true, lastName: true, profilePic: true }, comments: { userId: true } },
-    });
+    })) as unknown as BasePostOfFeedDTO[];
     return feed;
   }
 }
