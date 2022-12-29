028d9b6ad2f9da3303f43322a65b8d78ed65990f
Ray <raylu511@gmail.com>
2022-12-29 17:36:20 -0500

    Create feed.route.ts

---
+++ src/routes/feed.route.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { Routes } from '@/interfaces';
+import { AuthMiddleware } from '@/middlewares';
+import { FeedController } from '@/controllers';
+
+class FeedRoute implements Routes {
+  public readonly path = '/feed';
+  public readonly router = Router();
+  private feedController = new FeedController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, AuthMiddleware, this.feedController.getFeed);
+  }
+}
+
+export default FeedRoute;
