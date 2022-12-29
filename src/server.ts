695853c9f7f66b47fda4ba2662e518244f9c978f
Ray <raylu511@gmail.com>
2022-12-29 17:36:16 -0500

    Created feed route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import App from '@/app';
 import validateEnv from '@utils/validateEnv';
 import { IndexRoute, AuthRoute, UserRoute, PostRoute, CommentRoute } from '@/routes';
+import FeedRoute from './routes/feed.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute(), new PostRoute(), new UserRoute(), new CommentRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new PostRoute(), new UserRoute(), new CommentRoute(), new FeedRoute()]);
 
 app.listen();
