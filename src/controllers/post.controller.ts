f9e13a629aa9a66f98fe5201526ef9aa2f767b6a
Daywick <derekli11204@gmail.com>
2022-12-30 17:34:57 -0500

    change fixes

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response, Request } from 'express';
 import { PostService } from '@/services';
 import { BasePostDto, CreatePostRequestDto, UpdatePostRequestDto, UsersLikedPostDto } from '@/dtos';
 import { RequestWithUser } from '@/interfaces';
+import { HttpException } from '@/exceptions';
 
 class PostController {
   private postService = new PostService();
@@ -74,9 +75,9 @@ class PostController {
       const userId = req.user.id;
       const id = Number(req.params.post_id);
       await this.postService.likePost({ userId, id });
-      res.status(200).json({ message: 'Post Liked' });
+      res.status(201).json({ message: 'Post Liked' });
     } catch (error) {
-      next(error);
+      next(new HttpException(400, 'Already liked'));
     }
   };
 }
