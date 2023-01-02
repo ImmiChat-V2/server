319b0f0ca057b1339afaa4e45944696fa7b09352
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-01 23:48:14 -0500

    removed console.log

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,3 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, mDB_HOST, mDB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } =
   process.env;
-
-  console.log(SECRET_KEY)
