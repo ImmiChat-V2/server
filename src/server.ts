202afa3e553ae8c98a035ed1b29cac80e1c0727b
Daywick <derekli11204@gmail.com>
2023-01-02 15:19:04 -0500

    added route to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import App from '@/app';
 import validateEnv from '@utils/validateEnv';
-import { IndexRoute, AuthRoute, UserRoute, PostRoute, CommentRoute, FeedRoute, ConnectionRoute } from '@/routes';
+import { IndexRoute, AuthRoute, UserRoute, PostRoute, CommentRoute, FeedRoute, ConnectionRoute, ChatroomRoute } from '@/routes';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute(), new PostRoute(), new UserRoute(), new CommentRoute(), new FeedRoute(), new ConnectionRoute()]);
+const app = new App([
+  new IndexRoute(),
+  new AuthRoute(),
+  new PostRoute(),
+  new UserRoute(),
+  new CommentRoute(),
+  new FeedRoute(),
+  new ConnectionRoute(),
+  new ChatroomRoute(),
+]);
 
 app.listen();
