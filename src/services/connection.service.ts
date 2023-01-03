2ffb77655ee0fdbd41778e4f51b87fcd8c3c7c69
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-01-03 18:20:12 -0500

    find replaced with findOne

--- src/services/connection.service.ts
+++ src/services/connection.service.ts
@@ -26,24 +26,24 @@ class ConnectionService {
   }
   public async sendConnectionRequest(data: SendOrAcceptConnectionRequestDto): Promise<BaseConnectionsDto> {
     const { senderId, receiverId } = data;
-    const connectionRequest = await ConnectionsEntity.find({
+    const connectionRequest = await ConnectionsEntity.findOne({
       where: [
         { senderId, receiverId },
         { senderId: receiverId, receiverId: senderId },
       ],
     });
-    if (connectionRequest.length > 0) throw new HttpException(409, 'This connection already exists');
+    if (connectionRequest) throw new HttpException(409, 'This connection already exists');
     const newRequest: BaseConnectionsDto = await ConnectionsEntity.create({ ...data, connected: false }).save();
     return newRequest;
   }
 
   public async acceptConnectionRequest(data: SendOrAcceptConnectionRequestDto): Promise<BaseConnectionsDto> {
     const { senderId, receiverId } = data;
-    const connectionRequest = await ConnectionsEntity.find({
+    const connectionRequest = await ConnectionsEntity.findOne({
       where: { senderId, receiverId },
     });
-    if (connectionRequest.length < 1) throw new HttpException(404, "This connection doesn't exist");
-    const { id, connected } = connectionRequest[0];
+    if (!connectionRequest) throw new HttpException(404, "This connection doesn't exist");
+    const { id, connected } = connectionRequest;
     if (connected) throw new HttpException(409, 'Connection already exists');
     const acceptedConnectionRequest = await updateAndReturn<BaseConnectionsDto, { connected: boolean }>(id, { connected: true }, ConnectionsEntity);
     return acceptedConnectionRequest;
