5b26ab9ffe3f99850e603625b59f676092169a4b
Daywick <derekli11204@gmail.com>
2023-01-04 16:05:24 -0500

    git action

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -2,10 +2,26 @@ import App from '@/app';
 import request from 'supertest';
 import { UserRoute } from '@/routes';
 import { testpgDataSource } from '@databases';
-import { BaseUserDto } from '@dtos';
-import { UserEntity } from '@entities';
+import { AuthService } from '@/services';
+import { BaseUserResponseDTO } from '@/dtos';
 const userRoute = new UserRoute();
 const app = new App([userRoute]);
+const authService = new AuthService();
+const newDate = new Date();
+
+const tempUser: BaseUserResponseDTO = {
+  id: 1,
+  email: 'dsf@def.com',
+  firstName: 'fdsfsd',
+  lastName: 'fds',
+  language: 'mexico',
+  profilePic: 'src',
+  dateOfBirth: null,
+  updatedAt: newDate,
+  createdAt: newDate,
+};
+const token = authService.createToken(tempUser, 'access');
+const cookie = authService.createCookie(token, 'access');
 
 beforeEach(async () => {
   await testpgDataSource.initialize();
@@ -20,7 +36,7 @@ afterEach(async () => {
 describe('Testing User Endpoints', () => {
   describe('[GET] /users', () => {
     it('successfully get all users', async () => {
-      const response = await request(app.getServer()).get(`${userRoute.path}`);
+      const response = await request(app.getServer()).get(`${userRoute.path}`).set('Cookie', [cookie]);
       expect(response.statusCode).toBe(200);
     });
   });
