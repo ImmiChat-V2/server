79a768b9ab38b1e7a93642110d3eada2ee95eeff
Matthew Tan <tanmatt1515@gmail.com>
2023-01-08 16:47:37 -0500

    test for get specific user (#148)
    * passed all tests
    
    * created util function to send req with cookie
    
    * created path variable to use for each test
    
    * removed app import from utils
    
    * removed setTimeout in auth test

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,14 +1,14 @@
 import App from '@/app';
-import request from 'supertest';
 import { UserRoute } from '@/routes';
 import { pgDataSource } from '@databases';
-import { AuthService } from '@/services';
+import { UserEntity } from '@/entities';
 import { BaseUserResponseDTO } from '@/dtos';
+import { sendTestRequestWithCookie } from './utils/testUtils';
+
 const userRoute = new UserRoute();
 const app = new App([userRoute]);
-const authService = new AuthService();
+const server = app.getServer();
 
-const testCookie = authService.createTestCookie();
 beforeEach(async () => {
   await pgDataSource.initialize();
 });
@@ -19,8 +19,32 @@ afterEach(async () => {
 
 describe('Testing User Endpoints', () => {
   describe('[GET] /users', () => {
+    const path = `${userRoute.path}`;
     it('successfully get all users', async () => {
-      return await request(app.getServer()).get(`${userRoute.path}`).set('Cookie', [testCookie]).expect(200);
+      return await sendTestRequestWithCookie({ app: server, path, expectedStatusCode: 200 });
+    });
+  });
+  describe('[GET] /users/:id', () => {
+    const userData: BaseUserResponseDTO = {
+      id: 1,
+      firstName: 'firstName',
+      lastName: 'lastName',
+      email: 'test@email.com',
+      language: 'chinese',
+      dateOfBirth: null,
+      profilePic: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const path = `${userRoute.path}/${userData.id}`;
+
+    it('successfully gets a specific user', async () => {
+      UserEntity.findOne = jest.fn().mockImplementation(() => userData);
+      return await sendTestRequestWithCookie({ app: server, path, expectedStatusCode: 200 });
+    });
+    it('returns 409 status code if user is not found', async () => {
+      UserEntity.findOne = jest.fn().mockImplementation(() => false);
+      return await sendTestRequestWithCookie({ app: server, path, expectedStatusCode: 409 });
     });
   });
 });
