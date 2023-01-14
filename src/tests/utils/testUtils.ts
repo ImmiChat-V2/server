8339e1203590d31a1fe68610d6511a1e00825aa1
Ray <raylu511@gmail.com>
2023-01-14 18:13:16 -0500

    Updated request util

--- src/tests/utils/testUtils.ts
+++ src/tests/utils/testUtils.ts
@@ -1,20 +1,14 @@
 import { AuthService } from '@/services';
 import request from 'supertest';
-import { Express } from 'express';
 
 const authService = new AuthService();
 const testAccessCookie = authService.createTestCookie('access', 'access');
 
-export const sendTestRequestWithCookie = async ({
-  app,
-  path,
-  body,
-  expectedStatusCode,
-}: {
-  app: Express.Application;
-  path: string;
-  body?: any;
-  expectedStatusCode: number;
-}) => {
-  return await request(app).get(path).set('Cookie', [testAccessCookie]).send(body).expect(expectedStatusCode);
+type RequestWithCookieType = {
+  readonly app: Express.Application;
+  readonly path: string;
+};
+
+export const requestWithCookie = ({ app, path }: RequestWithCookieType) => {
+  return request(app).get(path).set('Cookie', [testAccessCookie]);
 };
