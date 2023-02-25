1b0d0bfc9fb53f4929d129510daa50be630fcf5c
Zohaib Manzoor <91208540+ZohaibManzoor00@users.noreply.github.com>
2023-02-25 14:56:31 -0800

    added query parameters to feed route (#175)

--- src/services/feed.service.ts
+++ src/services/feed.service.ts
@@ -2,11 +2,12 @@ import { BasePostOfFeedDTO, ProfileFeed } from '@/dtos';
 import { PostEntity } from '@/entities';
 
 class FeedService {
-  public async getFeed({ userId }: ProfileFeed): Promise<BasePostOfFeedDTO[]> {
+  public async getFeed({ userId, categoryName }: ProfileFeed): Promise<BasePostOfFeedDTO[]> {
     const feed: BasePostOfFeedDTO[] = (await PostEntity.find({
       relations: ['likes', 'comments', 'user'],
       where: {
         ...(userId && { userId }),
+        ...(categoryName && {categoryName})
       },
       select: {
         likes: { id: true },
